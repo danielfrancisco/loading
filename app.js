f049b27f1f0a85c57fdc33b008ea1789e7d7dc77
Daniel <dcampoverde@ifinetwork.com>
2023-04-11 15:15:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+
+let bar = document.getElementById("bar") 
+let barCon = document.getElementById("barCon") 
+let perdiv = document.getElementById("per") 
+let lo = document.getElementById("lo")
+lo.innerHTML="Loading"
+let barNum = 100
+
+let barWidth=String(barNum)+"px"
+
+bar.style.width=barWidth
+
+let counter = 1;
+
+function myLoop() {   
+  let per = (counter/5)*100
+  let perToCal=per*0.01 
+  barCon.style.width=String(barNum*perToCal)+"px"       
+  perdiv.innerHTML=String(per)+"%"
+  setTimeout(function() {
+      
+     counter++;                    
+    if (counter < 6) {           
+      myLoop();             
+    }                       
+  }, 1000)
+}
+
+function alo(){
+  setTimeout(function(){
+    switch(lo.innerHTML){
+      case "Loading": 
+      lo.innerHTML="Loading."
+      break
+      case "Loading.": 
+      lo.innerHTML="Loading.."
+      break
+      case "Loading..": 
+      lo.innerHTML="Loading..."
+      break
+      case "Loading...": 
+      lo.innerHTML="Loading"
+      break
+      default:
+    }
+
+      if (counter < 6 && barCon.style.width!=="100px") {        
+      alo();        
+    }     
+
+   },550)
+}
+console.log("haha")
+myLoop()
+alo()
+
+
+
